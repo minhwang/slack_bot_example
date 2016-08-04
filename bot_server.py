41e5ef380b038d44e841ff2529d38ab8f36af777
Min Hwang <min.hwang@linecorp.com>
2016-08-04 12:25:40 +0900

    Modified to respond to the command

--- bot_server.py
+++ bot_server.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import web
-import requests
+import json
 
 urls = (
     '/', 'Index',
@@ -17,8 +17,9 @@ class Index:
 class Hello:
     def POST(self):
         req_data = web.input()
-        requests.post(req_data.response_url, json={'text':'Hello, {0}'.format(req_data.user_name)})
-        pass
+        res = {'text':'Hello, {0}'.format(req_data.user_name)}
+        web.header('Content-Type', 'application/json')
+        return json.dumps(res)
 
 if __name__ == "__main__": 
     app = web.application(urls, globals())
