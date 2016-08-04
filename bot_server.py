2b7fe27751325a43896b5d3b56ecd458f3770763
Min Hwang <min.hwang@linecorp.com>
2016-08-04 10:43:52 +0900

    Added Hello command

--- bot_server.py
+++ bot_server.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import web
+import json
 
-urls = ('/', 'Index')
+urls = (
+    '/', 'Index',
+    '/hello', 'Hello'
+)
 
 class Index:
     def GET(self):
         return "Hello, world!"
     def POST(self):
         return "Post"
+
+class Hello:
+    def POST(self):
+        req_data = json.loads(web.data())
+        return 'Hello, {0}'.format(req_data['user_name'])
 
 if __name__ == "__main__": 
     app = web.application(urls, globals())
