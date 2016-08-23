d0855dafc933a4e9452dacd964dbad74aeb84449
Min Hwang <min.hwang@linecorp.com>
2016-08-23 21:08:24 +0900

    Restored

--- bot_server.py
+++ bot_server.py
@@ -44,6 +44,33 @@ class Hello:
         web.header('Content-Type', 'application/json')
         return json.dumps(a)
 
+class Bob:
+    def POST(self):
+        req_data = web.input()
+        a = {
+            'text': req_data.text,
+            'attachments': [
+                {
+                    'response_type': 'in_channel',
+                    'text': req_data.text,
+                    'fallback': 'fallback',
+                    'callback_id': 'callback_id',
+                    'color': '#3AA3E3',
+                    'attachment_type': 'default',
+                    'actions': [
+                        {
+                            'name': 'Join',
+                            'text': 'JOIN',
+                            'type': 'button',
+                            'value': 'join'
+                        }
+                    ]
+                }
+            ]
+        }
+        web.header('Content-Type', 'application/json')
+        return json.dumps(a)
+
 class YummyAction:
     def POST(self):
         pass
